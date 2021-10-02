d782492057ba86ccfbd561a43f0e4dda3e8157f6
Margo1212 <sirachenko944@gmail.com>
2021-10-02 12:01:00 +0200

    Update styles of weather-container

--- script.js
+++ script.js
@@ -12,17 +12,20 @@ let weather = {
         const { name } = data;
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
-        const { speed } = data.wind;
-        const { visibility } = data;
         const { feels_like } = data.main;
-        console.log(name, description, temp, humidity, speed)
-        document.querySelector('.city').innerText = name;
+        const { speed } = data.wind;
+        const { all } = data.clouds;
+        const { visibility } = data;
+        
+        console.log(name, description, temp, humidity, speed, all)
+        document.querySelector('.city').innerText = 'Current location: ' + name;
+        document.querySelector('.temperature').innerText = temp + '°C';
         document.querySelector('.description').innerText = description;
         document.querySelector('.icon').src = 'https://openweathermap.org/img/wn/' + icon + '@2x.png';
-        document.querySelector('.temperature').innerText = temp + '°C';
-        document.querySelector('.humidity').innerText = humidity;
+        document.querySelector('.real').innerText = 'RealFeel: ' + feels_like + '°C';
+        document.querySelector('.humidity').innerText = 'Humidity: ' + humidity;
         document.querySelector('.wind').innerText = speed;
-        document.querySelector('.real').innerText = 'RealFeel: ' + feels_like;
+        document.querySelector('.cloudcover').innerText = 'Cloudcover: ' + all + '%';
         document.querySelector('.visibility').innerText = 'Visibility: ' + visibility;
     }
 }
