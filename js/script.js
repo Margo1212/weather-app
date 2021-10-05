007a35b2e0a15864fccdbf86965c793eac76b726
Margo1212 <sirachenko944@gmail.com>
2021-10-05 12:35:24 +0200

    Update daily section

--- js/script.js
+++ js/script.js
@@ -23,21 +23,47 @@ let weather = {
         const { visibility } = list;
 
         //daily
-        
+        const list2 = data.list[8]
+        const temp2 = list2.main.temp
+        const description2 = list2.weather[0].description;
+
+        const list3 = data.list[16]
+        const temp3 = list3.main.temp
+        const description3 = list3.weather[0].description;
+
+        const list4 = data.list[24]
+        const temp4 = list4.main.temp
+        const description4 = list4.weather[0].description;
+
+
 
 
 
 
         console.log(name, description, temp, humidity, feels_like, speed, all, visibility )
+        //today
         document.querySelector('.city').innerText = 'Current location: ' + name + ', ';
         document.querySelector('.temperature').innerText = Math.round(temp) + '°C';
-        document.querySelector('.description').innerText = description;
-        
-        document.querySelector('.real').innerText = 'RealFeel: ' + Math.ceil(feels_like) + '°C';
+        document.querySelector('.description').innerText = description;        
+        document.querySelector('.real').innerText = 'RealFeel: ' + Math.round(feels_like) + '°C';
         document.querySelector('.humidity').innerText = 'Humidity: ' + humidity;
         document.querySelector('.wind').innerText = "Wind speed: " + speed + 'km/h';
         document.querySelector('.cloudcover').innerText = 'Cloudcover: ' + all + '%';
         document.querySelector('.visibility').innerText = 'Visibility: ' + visibility;
+
+        //daily
+
+        document.querySelector('.tomorrow .desc').innerText = description2;
+        document.querySelector('.tomorrow .temp').innerText = Math.round(temp2) + '°C';
+
+        document.querySelector('.dat .desc').innerText = description3;
+        document.querySelector('.dat .temp').innerText = Math.round(temp3) + '°C';
+ 
+        document.querySelector('.a2d .desc').innerText = description4;
+        document.querySelector('.a2d .temp').innerText = Math.round(temp4) + '°C';
+
+
+
     },
 
     search: function(){
